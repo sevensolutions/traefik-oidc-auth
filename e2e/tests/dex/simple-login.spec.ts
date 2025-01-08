2898b433abc7df6ec36f9ef3e378c0124e9916cc
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2025-01-08 13:18:12 +0100

    #34: Improve e2e test setup. (#47)
    * #34: Improve e2e test setup.
    * #34: More tests

---
+++ e2e/tests/dex/simple-login.spec.ts
@@ -0,0 +1,236 @@
+import { test, expect, Page, Response } from "@playwright/test";
+import * as dockerCompose from "docker-compose";
+import { configureTraefik } from "../../utils";
+
+//-----------------------------------------------------------------------------
+// Test Setup
+//-----------------------------------------------------------------------------
+
+test.use({
+  ignoreHTTPSErrors: true
+});
+
+test.beforeAll("Starting traefik", async () => {
+  await configureTraefik(`
+http:
+  services:
+    whoami:
+      loadBalancer:
+        servers:
+          - url: http://whoami:80
+
+  middlewares:
+    oidc-auth:
+      plugin:
+        traefik-oidc-auth:
+          LogLevel: DEBUG
+          Provider:
+            UrlEnv: "PROVIDER_URL"
+            ClientIdEnv: "CLIENT_ID"
+            ClientSecretEnv: "CLIENT_SECRET"
+            UsePkce: false
+
+  routers:
+    whoami:
+      entryPoints: ["web"]
+      rule: "HostRegexp(\`.+\`)"
+      service: whoami
+      middlewares: ["oidc-auth@file"]
+    whoami-secure:
+      entryPoints: ["websecure"]
+      tls: {}
+      rule: "HostRegexp(\`.+\`)"
+      service: whoami
+      middlewares: ["oidc-auth@file"]
+`);
+
+  await dockerCompose.upAll({
+    cwd: __dirname,
+    log: true
+  });
+});
+
+test.afterAll("Stopping traefik", async () => {
+  await dockerCompose.downAll({
+    cwd: __dirname,
+    log: true
+  });
+});
+
+//-----------------------------------------------------------------------------
+// Tests
+//-----------------------------------------------------------------------------
+
+test("login http", async ({ page }) => {
+  await page.goto("http://localhost:9080");
+
+  const response = await login(page, "admin@example.com", "password", "http://localhost:9080");
+
+  expect(response.status()).toBe(200);
+});
+
+test("login https", async ({ page }) => {
+  await page.goto("https://localhost:9443");
+
+  const response = await login(page, "admin@example.com", "password", "https://localhost:9443");
+
+  expect(response.status()).toBe(200);
+});
+
+// Seems like logout is not supported by dey yet :(
+// https://github.com/dexidp/dex/issues/1697
+// test("logout", async ({ page }) => {
+//   await page.goto("http://localhost:9080");
+
+//   const response = await login(page, "admin@example.com", "password", "http://localhost:9080");
+
+//   expect(response.status()).toBe(200);
+
+//   await page.goto("http://localhost:9080/logout");
+
+// });
+
+test("test headers", async ({ page }) => {
+  await configureTraefik(`
+http:
+  services:
+    whoami:
+      loadBalancer:
+        servers:
+          - url: http://whoami:80
+
+  middlewares:
+    oidc-auth:
+      plugin:
+        traefik-oidc-auth:
+          LogLevel: DEBUG
+          Provider:
+            UrlEnv: "PROVIDER_URL"
+            ClientIdEnv: "CLIENT_ID"
+            ClientSecretEnv: "CLIENT_SECRET"
+            UsePkce: false
+          Headers:
+            - Name: "Authorization"
+              Value: "{{\`Bearer: {{ .accessToken }}\`}}"
+            - Name: "X-Static-Header"
+              Value: "42"
+
+  routers:
+    whoami:
+      entryPoints: ["web"]
+      rule: "HostRegexp(\`.+\`)"
+      service: whoami
+      middlewares: ["oidc-auth@file"]
+`);
+
+  await page.goto("http://localhost:9080");
+
+  const response = await login(page, "admin@example.com", "password", "http://localhost:9080");
+
+  expect(response.status()).toBe(200);
+
+  const authHeaderExists = await page.locator(`text=Authorization: Bearer: ey`).isVisible();
+  expect(authHeaderExists).toBeTruthy();
+
+  const staticHeaderExists = await page.locator(`text=X-Static-Header: 42`).isVisible();
+  expect(staticHeaderExists).toBeTruthy();
+});
+
+test("test authorization", async ({ page }) => {
+  await configureTraefik(`
+http:
+  services:
+    whoami:
+      loadBalancer:
+        servers:
+          - url: http://whoami:80
+
+  middlewares:
+    oidc-auth:
+      plugin:
+        traefik-oidc-auth:
+          LogLevel: DEBUG
+          Provider:
+            UrlEnv: "PROVIDER_URL"
+            ClientIdEnv: "CLIENT_ID"
+            ClientSecretEnv: "CLIENT_SECRET"
+            UsePkce: false
+          Authorization:
+            AssertClaims:
+              - Name: email
+                AnyOf: ["admin@example.com", "alice@example.com"]
+
+  routers:
+    whoami:
+      entryPoints: ["web"]
+      rule: "HostRegexp(\`.+\`)"
+      service: whoami
+      middlewares: ["oidc-auth@file"]
+`);
+
+  await page.goto("http://localhost:9080");
+
+  const response = await login(page, "alice@example.com", "password", "http://localhost:9080");
+
+  expect(response.status()).toBe(200);
+});
+
+test("test authorization failing", async ({ page }) => {
+  await configureTraefik(`
+http:
+  services:
+    whoami:
+      loadBalancer:
+        servers:
+          - url: http://whoami:80
+
+  middlewares:
+    oidc-auth:
+      plugin:
+        traefik-oidc-auth:
+          LogLevel: DEBUG
+          Provider:
+            UrlEnv: "PROVIDER_URL"
+            ClientIdEnv: "CLIENT_ID"
+            ClientSecretEnv: "CLIENT_SECRET"
+            UsePkce: false
+          Authorization:
+            AssertClaims:
+              - Name: email
+                AnyOf: ["admin@example.com", "alice@example.com"]
+
+  routers:
+    whoami:
+      entryPoints: ["web"]
+      rule: "HostRegexp(\`.+\`)"
+      service: whoami
+      middlewares: ["oidc-auth@file"]
+`);
+
+  await page.goto("http://localhost:9080");
+
+  const response = await login(page, "bob@example.com", "password", "http://localhost:9080/oidc/callback**");
+
+  expect(response.status()).toBe(401);
+});
+
+//-----------------------------------------------------------------------------
+// Helper functions
+//-----------------------------------------------------------------------------
+
+async function login(page: Page, username: string, password: string, waitForUrl: string): Promise<Response> {
+  await page.waitForURL("http://localhost:5556/dex/auth**");
+
+  await page.locator(':text("Log in with Email")').click();
+
+  await page.locator("#login").fill(username);
+  await page.locator("#password").fill(password);
+
+  const responsePromise = page.waitForResponse(waitForUrl);
+
+  await page.locator('button:text("Login")').click();
+
+  const response = await responsePromise;
+
+  return response;
+}
