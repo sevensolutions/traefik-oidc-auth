53b3fab973a902597d87edcd65d4aef776db0600
sevensolutions <84123899+sevensolutions@users.noreply.github.com>
2026-08-16 17:47:41 +0200

    update output dir

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -24,7 +24,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['playwright-ctrf-json-reporter', {}]
+    ['playwright-ctrf-json-reporter', {
+      outputDir: './e2e/ctrf'
+    }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
